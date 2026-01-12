f1d99ef1c38e86bb550826cd69b467a261f8ba5b
Alraffi28 <mohammedalraffi@gmail.com>
2026-01-12 12:47:25 +0530

    hero created

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,64 +1,95 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './Home.css'
-import HeroImg from '../assets/hero_img.png'
+import sample from '../assets/img_portfolio.png'
 import { Link } from 'react-router-dom'
 import { IoIosBriefcase } from "react-icons/io"
-import Shuffle from './Shuffle'
 
 export default function Home() {
-  const [role , setRole] = useState(0)
-  const [char , setChar] = useState(0);
-  const [isDelete , setIsDelete] = useState(false)
-  const roles = ['Full stack developer', 'Front-end developer' , 'UI UX Designer']
-    useEffect(()=>{
-      const currentRole = roles[role]
-      let timer;
-      if (isDelete){
-        if(char > 0){
-          timer = setTimeout(()=>{
-            setChar((prev) => prev-1)
-          },100)
-        } else{
-          setIsDelete(false)
-          setRole((prev) => (prev + 1) % roles.length)
-        }
-      }else{
-        if(char < currentRole.length){
-          timer = setTimeout(() => {
-            setChar((prev)=>prev + 1)
-          }, 100);
-        } else{
-          timer = setTimeout(()=>{
-            setIsDelete(true)
-          },1500)
-        }
-      }
-      return () => clearTimeout(timer)
-    },[char,isDelete,role,roles])
   return (
-    <>
-    <section id='hero'>
-      <div className="hero-text">
-        <p className='hello'>Hello,</p>
-        <p className='introText'>I'm <span><Shuffle
-        text="Mohammed Alraffi"
-        shuffleDirection="right"
-        duration={0.45}
-        animationMode="evenodd"
-        shuffleTimes={1}
-        ease="power3.out"
-        stagger={0.03}
-        threshold={0.1}
-        triggerOnce={true}
-        triggerOnHover={true}
-        respectReducedMotion={true}
-      /></span><br /><span className='roles'>{roles[role].substring(0 , char)}</span><span className='typing'> |</span>
-      </p>
-        <p className='introPara'>I build responsive, accessible and user-friendly web applications. I love converting designs into clean, functional interfaces and bringing ideas to life with JavaScript.</p>
-        <Link style={{width : "fit-content"}}><button> <IoIosBriefcase size={18} />Hire Me</button></Link>
+    <section id='intro'>
+      <div className="introContent">
+          <span className='hello'>Hello,</span>
+          <span className='introText'>I'm <span className='introName'>Mohammed Alraffi</span><br />Full Stack Developer</span>
+          <p className="introPara">
+            I build responsive, accessible and user-friendly web applications. I love converting <br /> designs into clean, 
+            functional interfaces and bringing ideas to life with JavaScript.
+          </p>
+          <Link style={{width : "fit-content"}}><button> <IoIosBriefcase size={18} />Hire Me</button></Link>
       </div>
-        <img src={HeroImg} alt="" className="hero-img"/>
+      <img src={sample} alt="" className='bg'/>
     </section>
-    </>
   )
 }
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import './Home.css'
+// import HeroImg from '../assets/hero_img.png'
+// 
+// import { Link } from 'react-router-dom'
+// 
+// import Shuffle from './Shuffle'
+
+// export default function Home() {
+//   const [role , setRole] = useState(0)
+//   const [char , setChar] = useState(0);
+//   const [isDelete , setIsDelete] = useState(false)
+//   const roles = ['Full stack developer', 'Front-end developer' , 'UI UX Designer']
+//     useEffect(()=>{
+//       const currentRole = roles[role]
+//       let timer;
+//       if (isDelete){
+//         if(char > 0){
+//           timer = setTimeout(()=>{
+//             setChar((prev) => prev-1)
+//           },100)
+//         } else{
+//           setIsDelete(false)
+//           setRole((prev) => (prev + 1) % roles.length)
+//         }
+//       }else{
+//         if(char < currentRole.length){
+//           timer = setTimeout(() => {
+//             setChar((prev)=>prev + 1)
+//           }, 100);
+//         } else{
+//           timer = setTimeout(()=>{
+//             setIsDelete(true)
+//           },1500)
+//         }
+//       }
+//       return () => clearTimeout(timer)
+//     },[char,isDelete,role,roles])
+//   return (
+//     <>
+//     <section id='hero'>
+//       <div className="hero-text">
+//         <p className='hello'>Hello,</p>
+//         <p className='introText'>I'm <span><Shuffle
+//         text="Mohammed Alraffi"
+//         shuffleDirection="right"
+//         duration={0.45}
+//         animationMode="evenodd"
+//         shuffleTimes={1}
+//         ease="power3.out"
+//         stagger={0.03}
+//         threshold={0.1}
+//         triggerOnce={true}
+//         triggerOnHover={true}
+//         respectReducedMotion={true}
+//       /></span><br /><span className='roles'>{roles[role].substring(0 , char)}</span><span className='typing'> |</span>
+//       </p>
+//         <p className='introPara'>I build responsive, accessible and user-friendly web applications. I love converting designs into clean, functional interfaces and bringing ideas to life with JavaScript.</p>
+//         <Link style={{width : "fit-content"}}><button> <IoIosBriefcase size={18} />Hire Me</button></Link>
+//       </div>
+//       <div className="homeImg">
+//         <img src={sample} alt="" className="hero-img"/>
+//       </div>
+//     </section>
+//     </>
+//   )
+// }
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./Navbar.css"
 import {Link} from 'react-router-dom'
+import LogoImg from '../assets/my_logo.png'
+import { LuMessageCircle } from "react-icons/lu"
 
 export default function Navbar() {
   return (
     <>
-        <nav className="navbar">
+    <nav>
+      <img src={LogoImg} alt="logo" className='logo'/>
+      <div className="desktopMenu">
+          <Link className='desktopLinks'>Home</Link>
+          <Link className='desktopLinks'>About</Link>
+          <Link className='desktopLinks'>Projects</Link>
+      </div>
+      <button className="desktopBtn"><LuMessageCircle size={18} />Contact Me</button>
+    </nav>
+        {/* <nav className="navbar">
             Portfolio
         <div className="nav-links">
             <Link to="/" className='desktopLinks'>Home</Link>
@@ -13,7 +24,7 @@ export default function Navbar() {
             <Link className='desktopLinks'>Projects</Link>
         </div>
         <button>Contact Me</button>
-        </nav>
+        </nav> */}
     </>
   )
 }
